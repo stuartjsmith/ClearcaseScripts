53ab33720629bd5a8c27307624da1b1ce71cf5a4
Stuart Smith <stu@cityfifteen.co.uk>
2016-03-02 15:47:46 +0000

    Added files via upload

--- CreateView.bat
+++ CreateView.bat
@@ -6,11 +6,12 @@ SET CONFIG_SPEC=Latest
 if NOT "%4"=="" SET CONFIG_SPEC=%4
 SET WINKIN=n
 if /I "%5"=="true" SET WINKIN=
+SET PATH_TO_CONFIG_SPECS=ABSOLUTE_PATH_TO_FOLDER_CONTAINING_YOUR_CONFIG_SPECS
  
 echo Creating new view at %CLEARCASE_ROOT%\%VIEW_NAME% on %COMPUTERNAME% with Config Spec %CONFIG_SPEC% in Region %REGION%
 if "%WINKIN%"=="n" (echo WINKIN IS DISABLED ON THIS VIEW) else (echo WINKIN IS ENABLED ON THIS VIEW)
 
 cleartool mkview -tag %VIEW_NAME% -region %REGION% -%WINKIN%shareable_dos \\%COMPUTERNAME%\views\%VIEW_NAME%.vws
 cd /d %CLEARCASE_ROOT%\%VIEW_NAME%
-if /I NOT "%CONFIG_SPEC%"=="latest" cleartool setcs %CONFIG_SPEC%
+if /I NOT "%CONFIG_SPEC%"=="latest" cleartool setcs %PATH_TO_CONFIG_SPECS%\%CONFIG_SPEC%
 cleartool catcs
