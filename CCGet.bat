c41ac6ab843bbc97839673922e5f26f7edf557c1
Stuart Smith <stu@cityfifteen.co.uk>
2016-03-02 15:50:57 +0000

    Added files via upload

--- CCGet.bat
+++ CCGet.bat
@@ -1,14 +1,15 @@
-echo Usage: CCGet.bat PATH_CONTAINING_FOLDER FOLDER BRANCH
+echo Usage: CCGet.bat PATH_CONTAINING_FOLDER FOLDER REGION BRANCH
 echo Pulls code from Clearcase onto your local disk
 
 SET CURRENT_DIR=%CD%
 SET PATH_CONTAINING_FOLDER=%1
 SET FOLDER=%2
+SET REGION=%3
 SET CONFIG_SPEC=Latest
-if NOT "%3"=="" SET CONFIG_SPEC=%3
+if NOT "%4"=="" SET CONFIG_SPEC=%4
 SET VIEW_NAME=%USERNAME%_CCGet_Temp
 
-call "%~dp0\CreateView" M: %VIEW_NAME% vpd_ches %CONFIG_SPEC% 
+call "%~dp0\CreateView" M: %VIEW_NAME% %REGION% %CONFIG_SPEC% 
 cd /d %CURRENT_DIR%
 robocopy M:\%VIEW_NAME%\%PATH_CONTAINING_FOLDER%\%FOLDER% %CURRENT_DIR%\%FOLDER% /MIR
 call cmd /K "%~dp0\DeleteView" %VIEW_NAME%
